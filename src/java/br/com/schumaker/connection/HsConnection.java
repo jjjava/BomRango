b4bac534ff5b4d25d20b471c0fce5236974f6f65
hudson <hudson@192.168.0.1>
2015-01-13 21:24:19 -0200

    #connection error

--- src/java/br/com/schumaker/connection/HsConnection.java
+++ src/java/br/com/schumaker/connection/HsConnection.java
@@ -17,8 +17,8 @@ public class HsConnection {
         Connection conn = null;
         try {
             Driver drv = (Driver) Class.forName("com.mysql.jdbc.Driver").newInstance();
-            String URL = "jdbc:mysql://xx:3306/compras";
-            conn = DriverManager.getConnection(URL, "xx", "xx");
+            String URL = "jdbc:mysql://x:3306/compras";
+            conn = DriverManager.getConnection(URL, "x", "x");
 
         } catch (ClassNotFoundException ex) {
             System.err.println(ex);
