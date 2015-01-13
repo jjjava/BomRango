b600aad15f79531cf0293664172ff012795740d0
hudson.sales <hudson.sales@SPW9712NTW7P.sondait.com.br>
2015-01-13 14:20:16 -0300

    MercadoDao

--- src/java/br/com/schumaker/dao/impl/MercadoDaoImpl.java
+++ src/java/br/com/schumaker/dao/impl/MercadoDaoImpl.java
@@ -2,6 +2,8 @@ package br.com.schumaker.dao.impl;
 
 import br.com.schumaker.connection.HsConnection;
 import br.com.schumaker.dao.MercadoDao;
+import br.com.schumaker.model.Cidade;
+import br.com.schumaker.model.Estado;
 import br.com.schumaker.model.Mercado;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -33,6 +35,14 @@ public class MercadoDaoImpl implements MercadoDao {
                 mercado.setEndereco(rs.getString("endereco"));
                 mercado.setTelefone(rs.getString("telefone"));
                 mercado.setSite(rs.getString("site"));
+                mercado.setCartoes(rs.getString("cartoes"));
+                mercado.setCnpj(rs.getString("cnpj"));
+                mercado.setHorarioFunc(rs.getString("horariofunc"));
+                mercado.setFundacao(rs.getString("fundacao"));
+                mercado.setSobre(rs.getString("sobre"));
+                //ojbetos
+                mercado.setCidade(getMyCidade(rs.getInt("idcidade")));
+                mercado.setEstado(getMyEstado(rs.getInt("idestado")));
             }
         } catch (SQLException e) {
             System.err.println(e);//throw new RuntimeException(e);
@@ -62,6 +72,14 @@ public class MercadoDaoImpl implements MercadoDao {
                 mercado.setEndereco(rs.getString("endereco"));
                 mercado.setTelefone(rs.getString("telefone"));
                 mercado.setSite(rs.getString("site"));
+                mercado.setCartoes(rs.getString("cartoes"));
+                mercado.setCnpj(rs.getString("cnpj"));
+                mercado.setHorarioFunc(rs.getString("horariofunc"));
+                mercado.setFundacao(rs.getString("fundacao"));
+                mercado.setSobre(rs.getString("sobre"));
+                //ojbetos
+                mercado.setCidade(getMyCidade(rs.getInt("idcidade")));
+                mercado.setEstado(getMyEstado(rs.getInt("idestado")));
                 //---add na lista
                 mercados.add(mercado);
             }
@@ -87,12 +105,20 @@ public class MercadoDaoImpl implements MercadoDao {
             ResultSet rs = pst.executeQuery();
             while (rs.next()) {
                 Mercado mercado = new Mercado();
-                mercado.setId(rs.getInt("id"));
+               mercado.setId(rs.getInt("id"));
                 mercado.setIdDensidade(rs.getInt("iddensidade"));
                 mercado.setNome(rs.getString("nome"));
                 mercado.setEndereco(rs.getString("endereco"));
                 mercado.setTelefone(rs.getString("telefone"));
                 mercado.setSite(rs.getString("site"));
+                mercado.setCartoes(rs.getString("cartoes"));
+                mercado.setCnpj(rs.getString("cnpj"));
+                mercado.setHorarioFunc(rs.getString("horariofunc"));
+                mercado.setFundacao(rs.getString("fundacao"));
+                mercado.setSobre(rs.getString("sobre"));
+                //ojbetos
+                mercado.setCidade(getMyCidade(rs.getInt("idcidade")));
+                mercado.setEstado(getMyEstado(rs.getInt("idestado")));
                 //---add na lista
                 mercados.add(mercado);
             }
@@ -107,22 +133,30 @@ public class MercadoDaoImpl implements MercadoDao {
         }
         return mercados;
     }
-    
+
     public List<Mercado> listarByDensidade(int idDensidade) {
         List<Mercado> mercados = new ArrayList<Mercado>();
-        String sql = "select * from compras.mercado where mercado.iddensidade ="+idDensidade+" order by mercado.nome";
+        String sql = "select * from compras.mercado where mercado.iddensidade =" + idDensidade + " order by mercado.nome";
         Connection conn = HsConnection.getConnection();
         try {
             PreparedStatement pst = conn.prepareStatement(sql);
             ResultSet rs = pst.executeQuery();
             while (rs.next()) {
                 Mercado mercado = new Mercado();
-                mercado.setId(rs.getInt("id"));
+               mercado.setId(rs.getInt("id"));
                 mercado.setIdDensidade(rs.getInt("iddensidade"));
                 mercado.setNome(rs.getString("nome"));
                 mercado.setEndereco(rs.getString("endereco"));
                 mercado.setTelefone(rs.getString("telefone"));
                 mercado.setSite(rs.getString("site"));
+                mercado.setCartoes(rs.getString("cartoes"));
+                mercado.setCnpj(rs.getString("cnpj"));
+                mercado.setHorarioFunc(rs.getString("horariofunc"));
+                mercado.setFundacao(rs.getString("fundacao"));
+                mercado.setSobre(rs.getString("sobre"));
+                //ojbetos
+                mercado.setCidade(getMyCidade(rs.getInt("idcidade")));
+                mercado.setEstado(getMyEstado(rs.getInt("idestado")));
                 //---add na lista
                 mercados.add(mercado);
             }
@@ -136,5 +170,13 @@ public class MercadoDaoImpl implements MercadoDao {
             }
         }
         return mercados;
+    }
+
+    private Estado getMyEstado(Integer id) {
+        return new EstadoDaoImpl().obter(id);
+    }
+
+    private Cidade getMyCidade(Integer id) {
+        return new CidadeDaoImpl().obter(id);
     }
 }
