9997722385a35151fc22be869fd1ef1b2c83d7a2
hudson.sales <hudson.sales@SPW9712NTW7P.sondait.com.br>
2015-01-13 10:26:14 -0300

    #UsuarioDaoImpl

---
+++ src/java/br/com/schumaker/dao/impl/UsuarioDaoImpl.java
@@ -0,0 +1,130 @@
+package br.com.schumaker.dao.impl;
+
+import br.com.schumaker.connection.HsConnection;
+import br.com.schumaker.dao.UsuarioDao;
+import br.com.schumaker.model.Usuario;
+import br.com.schumaker.util.HsEncryption;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author hudson schumaker HStudio - @BomRango 13/01/2015
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class UsuarioDaoImpl implements UsuarioDao {
+
+    @Override
+    public Usuario obter(Integer id) {
+        String sql = "select * from compras.usuario where usuario.id = " + id;
+        Connection conn = HsConnection.getConnection();
+        Usuario usuario = new Usuario();
+        try {
+            PreparedStatement pst = conn.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                usuario.setId(rs.getInt("id"));
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setSenha(rs.getString("senha"));//duvida se carregar ou nao 
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+        return usuario;
+    }
+
+    @Override
+    public List<Usuario> listar() {
+        String sql = "select * from compras.usuario order by usuario.nome";
+        Connection conn = HsConnection.getConnection();
+        List<Usuario> usuarios = new ArrayList<Usuario>();
+        try {
+            PreparedStatement pst = conn.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                Usuario usuario = new Usuario();
+                usuario.setId(rs.getInt("id"));
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setSenha(rs.getString("senha"));//duvida se carregar ou nao 
+                //adiciona a lista
+                usuarios.add(usuario);
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+        return usuarios;
+    }
+
+    @Override
+    public List<Usuario> like(String nome) {
+        String sql = "select * from compras.usuario where usuario.nome like '%" + nome + "%' order by usuario.nome";
+        Connection conn = HsConnection.getConnection();
+        List<Usuario> usuarios = new ArrayList<Usuario>();
+        try {
+            PreparedStatement pst = conn.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                Usuario usuario = new Usuario();
+                usuario.setId(rs.getInt("id"));
+                usuario.setNome(rs.getString("nome"));
+                usuario.setEmail(rs.getString("email"));
+                usuario.setSenha(rs.getString("senha"));//duvida se carregar ou nao 
+                //adiciona a lista
+                usuarios.add(usuario);
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+        return usuarios;
+    }
+
+    @Override
+    public boolean validar(String email, String password) {
+        boolean validado = false;
+        String cryptEmail = HsEncryption.encrypt(email);
+        String cryptPassword = HsEncryption.encrypt(password);
+        String sql = "select * from compras.usuario where usuario.email = '" + cryptEmail + "' and usuario.senha = '" + cryptPassword + "'";
+        Connection conn = HsConnection.getConnection();
+        try {
+            PreparedStatement pst = conn.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                validado = true;
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+        return validado;
+    }
+}
