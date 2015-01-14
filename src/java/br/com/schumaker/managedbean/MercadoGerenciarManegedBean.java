539fb92ad89b686196454f42e8d0a5754a7db4e9
hudson.sales <hudson.sales@SPW9712NTW7P.sondait.com.br>
2015-01-14 10:31:02 -0300

    Redirect enricar mercado bean

--- src/java/br/com/schumaker/managedbean/MercadoGerenciarManegedBean.java
+++ src/java/br/com/schumaker/managedbean/MercadoGerenciarManegedBean.java
@@ -1,6 +1,8 @@
 package br.com.schumaker.managedbean;
 
+import br.com.schumaker.dao.impl.MercadoDaoImpl;
 import br.com.schumaker.model.Cliente;
+import br.com.schumaker.model.Mercado;
 import java.io.IOException;
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
@@ -18,16 +20,39 @@ import javax.servlet.http.HttpSession;
 @ViewScoped
 public class MercadoGerenciarManegedBean implements Serializable {
 
+    private Mercado mercado;
+    private Cliente cliente;
+
     public MercadoGerenciarManegedBean() {
         FacesContext fc = FacesContext.getCurrentInstance();
         HttpSession session = (HttpSession) fc.getExternalContext().getSession(false);
-        Cliente cliente = (Cliente) session.getAttribute("Cliente");
-        if (Cliente == null) {
+        cliente = (Cliente) session.getAttribute("Cliente");
+        if (cliente == null) {
             try {
-                FacesContext.getCurrentInstance().getExternalContext().redirect("faces/gerenciar.xhtml");
+                System.out.println("sessão vazia redirecionando...");
+                FacesContext.getCurrentInstance().getExternalContext().redirect("../gerenciar.xhtml");
             } catch (IOException ex) {
                 System.err.println(ex);
             }
+        } else {
+            MercadoDaoImpl mercadoDaoImpl = new MercadoDaoImpl();
+            mercado = mercadoDaoImpl.obter(cliente.getIdMercado());
         }
+    }
+
+    public Mercado getMercado() {
+        return mercado;
+    }
+
+    public void setMercado(Mercado mercado) {
+        this.mercado = mercado;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
     }
 }
