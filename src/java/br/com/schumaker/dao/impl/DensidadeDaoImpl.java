a5adc47e114c0fda911a1376f4d34e7829769826
hudson.sales <hudson.sales@SPW9712NTW7P.sondait.com.br>
2015-01-15 14:23:55 -0300

    DensidadedaoImpl - verificar, inserir

--- src/java/br/com/schumaker/dao/impl/DensidadeDaoImpl.java
+++ src/java/br/com/schumaker/dao/impl/DensidadeDaoImpl.java
@@ -3,6 +3,7 @@ package br.com.schumaker.dao.impl;
 import br.com.schumaker.connection.HsConnection;
 import br.com.schumaker.dao.DensidadeDao;
 import br.com.schumaker.model.Densidade;
+import br.com.schumaker.model.Fabricante;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -94,5 +95,47 @@ public class DensidadeDaoImpl implements DensidadeDao{
             }
         }
         return densidades;
+    }
+    
+     public boolean verifyName(String nome) {
+        boolean validado = false;
+        String sql = "select * from compras.densidade where densidade.nome = '" + nome + "'";
+        Connection conn = HsConnection.getConnection();
+        try {
+            PreparedStatement pst = conn.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                validado = true;
+            }
+        } catch (SQLException e) {
+            System.err.println(e);
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+        return validado;
+    }
+
+    public void inseri(Densidade densidade) {
+        String sql = "insert into compras.densidade (nome) values (?)";
+        Connection conn = HsConnection.getConnection();
+        PreparedStatement pst = null;
+        try {
+            pst = conn.prepareStatement(sql);
+            pst.setString(1, densidade.getNome());
+            pst.execute();
+        } catch (SQLException e) {
+            System.err.println(e);
+        } finally {
+            try {
+                pst.close();
+                conn.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
     }
 }
