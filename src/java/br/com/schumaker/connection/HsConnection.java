7afb06dfe692f7a206cf58d66683f55f8841a444
hudson <hudson@192.168.0.1>
2015-01-16 23:02:23 -0200

    pequenos acertos de libs#2

--- src/java/br/com/schumaker/connection/HsConnection.java
+++ src/java/br/com/schumaker/connection/HsConnection.java
@@ -17,7 +17,7 @@ public class HsConnection {
         Connection conn = null;
         try {
             Driver d = (Driver) Class.forName("com.mysql.jdbc.Driver").newInstance();
-
+            
         } catch (ClassNotFoundException ex) {
             System.err.println(ex);
         } catch (SQLException ex) {
