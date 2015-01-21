2a70ba0ffc3590dd14f06e677d9e053bc501a1ac
hudson.sales <hudson.sales@SPW9712NTW7P.sondait.com.br>
2015-01-21 11:07:20 -0300

    HsFiles -> rootDir, dirs...

---
+++ src/java/br/com/schumaker/hsfiles/HsFiles.java
@@ -0,0 +1,110 @@
+package br.com.schumaker.hsfiles;
+
+import java.io.File;
+
+/**
+ *
+ * @author hudson schumaker HStudio - @BomRango 21/01/2015
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class HsFiles {
+
+    private static String OS = System.getProperty("os.name").toLowerCase();
+    private final String rootDirWin = "C:/Hstudio";
+    private final String rootDirUnix = "/Hstudio";
+    private final String rootDirOsX = "/Applications/Hstudio";
+    private final String mercado = "mercado";
+    private final String usuario = "ususario";
+
+    public boolean criarDirMercado(String nome) {
+        if (isWindows()) {
+            File f = new File(rootDirWin + "/" + mercado + "/" + nome);
+            return f.mkdir();
+        } else if (isUnix()) {
+            File f = new File(rootDirUnix + "/" + mercado + "/" + nome);
+            return f.mkdir();
+        } else if (isMac()) {
+            File f = new File(rootDirOsX + "/" + mercado + "/" + nome);
+            return f.mkdir();
+        } else {
+            return false;
+        }
+    }
+
+    public boolean criarDirUsuario(String nome) {
+        if (isWindows()) {
+            File f = new File(rootDirWin + "/" + mercado + "/" + nome);
+            return f.mkdir();
+        } else if (isUnix()) {
+            File f = new File(rootDirUnix + "/" + mercado + "/" + nome);
+            return f.mkdir();
+        } else if (isMac()) {
+            File f = new File(rootDirOsX + "/" + mercado + "/" + nome);
+            return f.mkdir();
+        } else {
+            return false;
+        }
+    }
+
+    public boolean criarRaizDirSistema() {
+        if (isWindows()) {
+            File f = new File(rootDirWin);
+            return f.mkdir();
+        } else if (isUnix()) {
+            File f = new File(rootDirUnix);
+            return f.mkdir();
+        } else if (isMac()) {
+            File f = new File(rootDirOsX);
+            return f.mkdir();
+        } else {
+            return false;
+        }
+    }
+
+    public boolean criarDirRaizMercado() {
+        if (isWindows()) {
+            File f = new File(rootDirWin + "/" + mercado);
+            return f.mkdir();
+        } else if (isUnix()) {
+            File f = new File(rootDirUnix + "/" + mercado);
+            return f.mkdir();
+        } else if (isMac()) {
+            File f = new File(rootDirOsX + "/" + mercado);
+            return f.mkdir();
+        } else {
+            return false;
+        }
+    }
+
+    public boolean criarDirRaizUsuarios() {
+        if (isWindows()) {
+            File f = new File(rootDirWin + "/" + usuario);
+            return f.mkdir();
+        } else if (isUnix()) {
+            File f = new File(rootDirUnix + "/" + usuario);
+            return f.mkdir();
+        } else if (isMac()) {
+            File f = new File(rootDirOsX + "/" + usuario);
+            return f.mkdir();
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isWindows() {
+        return (OS.contains("win"));
+    }
+
+    public static boolean isMac() {
+        return (OS.contains("mac"));
+    }
+
+    public static boolean isUnix() {
+        return (OS.contains("nix") || OS.contains("nux") || OS.contains("aix"));
+    }
+
+    public static boolean isSolaris() {
+        return (OS.contains("sunos"));
+    }
+}
---
+++ src/java/br/com/schumaker/util/HsUtil.java
@@ -0,0 +1,12 @@
+package br.com.schumaker.util;
+
+/**
+ *
+ * @author hudson schumaker HStudio - @BomRango 21/01/2015
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class HsUtil {
+
+   
+}
